ca1937184406b6fd7c85829285dbe8354b349803
Tyeson Nguyen <133926503+VeeksTye@users.noreply.github.com>
2024-10-15 22:04:05 -0700

    Update Deck.h

--- 17C Project 1/Deck.h
+++ 17C Project 1/Deck.h
@@ -1,1 +1,18 @@
+#ifndef DECK_H
+#define DECK_H
+
+//include Card.h to make Deck aware of the Card class
+#include "Card.h"
+
+class Deck
+{
+private:
+    Card arrCards[52];
+    
+public:
+    void SetUpCards();
+    
+};
+
+#endif /* DECK_H */
 
