9bc4c2d5c874e3617f7f1f69d3e9a0028692ebe7
Tyeson Nguyen <133926503+VeeksTye@users.noreply.github.com>
2024-10-18 15:42:56 -0700

    Update Card.h

--- 17C Project 1/Card.h
+++ 17C Project 1/Card.h
@@ -9,9 +9,11 @@ public:
     CardNames name;
     Suits suit;
     int value;
+    int playOrderIndex;
     
+    void PrintSuit();
+    bool CanPlay(const &, const &);
 };
 
 #endif /* CARD_H */
-
 
