cb9f47b74651b43a495395f5c5fb627a0e6edec2
Tyeson Nguyen <133926503+VeeksTye@users.noreply.github.com>
2024-10-24 22:01:51 -0700

    Update Deck.h

--- 17C Project 1/Deck.h
+++ 17C Project 1/Deck.h
@@ -2,13 +2,16 @@
 #define DECK_H
 
 //include Card.h to make Deck aware of the Card class
+//include Player.h to set player hand from deck class
 #include "Card.h"
+#include "Player.h"
 
 class Deck
 {
 private:
     Card* arrCards[52];
-    
+    Player player[4];
+    int choice;
 public:
     //default constructor
     Deck();
@@ -16,8 +19,13 @@ public:
     
     void SetUpCards();
     void PrintAll();
-    int ShuffleCards();
+    void SetPlayerHand();
+    void Menu(int);
+    void Instructions();
+    
+    int randomfunc(int);
     int DealCards();
+    
 };
 
 #endif /* DECK_H */
