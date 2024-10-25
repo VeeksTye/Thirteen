88b8e58032521bcd1156efb9f60a8ec0f0d10e86
Tyeson Nguyen <133926503+VeeksTye@users.noreply.github.com>
2024-10-24 22:00:27 -0700

    Update main.cpp

--- 17C Project 1/main.cpp
+++ 17C Project 1/main.cpp
@@ -5,57 +5,21 @@
  * Program: Tiến lên (Thirteen)
  */
 
-#include <cstdlib>
 #include <iostream>
-#include <string>
 #include "Deck.h"
 #include "Card.h"
+#include "CardTypes.h"
 
 using namespace std;
 
 //function declaration
-void Deck::SetUpCards()
-{
-    Card arrCards[52];
-    
-    //loops setup to initialize every card
-    for (int col = (int)Suits::CLUBS; col <= (int)Suits::HEARTS; col++)
-    {
-        for (int rows = (int)CardNames::THREE; rows <= (int)CardNames::TWO; rows++)
-        {
-            Card c;
-            c.suit = (Suits)col;
-            c.name = (CardNames)rows; 
-            
-            if (c.name == CardNames::JACK)
-            {
-                c.value = 10;
-            }
-            if (c.name == CardNames::QUEEN)
-            {
-                c.value = 10;
-            }
-            if (c.name == CardNames::KING)
-            {
-                c.value = 10;
-            }
-            else
-            {
-                c.value = (int)c.name;
-            }
-            
-            cout << (int)c.name << " of " << (int)c.value << endl;
-            
-            int index = (13 * col) + (rows-1);
-            arrCards[index] = c;
-        }
-    }
-}
 
 int main() 
 {
+    int choice = 0;
     Deck deck;
-    deck.SetUpCards();
     
+    deck.Menu(choice);
+    deck.PrintAll();
     return 0;
 }
