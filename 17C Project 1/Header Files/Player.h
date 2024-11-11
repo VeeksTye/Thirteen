086e1da3fe7d693fc75bfb8a85540bd00e544ff1
Tyeson Nguyen <133926503+VeeksTye@users.noreply.github.com>
2024-11-10 21:30:50 -0800

    Update Player.h

--- 17C Project 1/Header Files/Player.h
+++ 17C Project 1/Header Files/Player.h
@@ -2,6 +2,7 @@
 #define PLAYER_H
 
 #include <string>
+#include <list>
 
 class Player
 {
@@ -9,13 +10,19 @@ private:
     std::string name;
     int score;
     bool turn, skipped;
-    
+
 public:
     //default constructor
     Player();
     Player(std::string, int, bool, bool);
     
     //functions
+    std::list<int>& GetHand();
+    void AddCard(int card);
+    void RemoveCard(int card);
+    void sortHand();
+    void beginHand();
+    
     void getName(std::string);
     void getScore(int);
     void getTurn(bool);
