c98819e90a78fea502f6dec9f3050a532622bd13
Tyeson Nguyen <133926503+VeeksTye@users.noreply.github.com>
2024-11-11 23:22:27 -0800

    Delete 17c Project 1/build/Debug/Cygwin-Windows directory

--- 17c Project 1/build/Debug/Cygwin-Windows/Card.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/Cygwin-Windows/Card.o: Card.cpp Card.h CardTypes.h Player.h
-Card.h:
-CardTypes.h:
-Player.h:
--- 17c Project 1/build/Debug/Cygwin-Windows/Player.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/Cygwin-Windows/Player.o: Player.cpp Player.h
-Player.h:
